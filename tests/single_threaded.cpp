26e8a4d4676d89a9f49f70ee685d32e4f58d0da6
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-03 14:17:07 +0530

    removed all simple compile time errors

--- tests/single_threaded.cpp
+++ tests/single_threaded.cpp
@@ -28,112 +28,140 @@ std::string random_string(size_t length) {
     std::generate_n(str.begin(), length, randchar);
     return str;
 }
+template <typename T>
+auto getNthIterator(const map<T, T> &mp, int n) {
+    int c = 0;
+
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        if (c == n) {
+            return it;
+        }
+        c++;
+    }
+
+    return mp.end();
+}
+
+typedef std::map<Slice, Slice> mpss;
+
+int getRandomIndex(const mpss &mp) {
+    return rand() % (int)mp.size();
+}
+
+auto getRandomIterator(const mpss &mp) {
+    return getNthIterator(mp, getRandomIndex(mp));
+}
 
 int main() {
-    Slice key_slice, val_slice;
-    key_slice.size = KEY_SIZE;
-    val_slice.size = KEY_SIZE;
-
     kvStore kv(STORE_SIZE);
-    std::map<Slice, Slice> m;
+    string k, v;
+    mpss m;
 
     for (int i = 0; i < TEST_SIZE; i++) {
+        Slice key_slice, val_slice;
+        key_slice.size = KEY_SIZE;
+        val_slice.size = KEY_SIZE;
+
         switch (rand() % 5) {
             case 0:
                 // test kvStore->get
                 std::cout << "[GET] ";
                 if (m.size()) {
-                    key_slice.data = m[m.begin() + rand() % m.size() - 1];
+                    auto it = getRandomIterator(m);
+                    key_slice.data = (it->first).data;
+
+                    std::cout << "key: " << key_slice.data
+                              << " val: " << val_slice.data << " ";
+
                     if (kv.get(key_slice, val_slice)) {
                         if (m[key_slice] == val_slice)
-                            std::cout << "key: " << key_slice.data
-                                      << " val: " << val_slice.data
-                                      << " succeeded\n";
+                            std::cout << "succeeded\n";
                         else
-                            std::cout << "key: " << key_slice.data
-                                      << " val: " << val_slice.data
-                                      << " mismatch\n";
+                            std::cout << "mismatch\n";
                     } else {
-                        std::cout << "key: " << key_slice.data
-                                  << " val: " << val_slice.data << " failed\n";
+                        std::cout << "failed\n";
                     }
                 } else {
                     std::cout << "store empty\n";
                 }
+                break;
             case 1:
                 // test kvStore->put
-                key_slice.data = random_string(KEY_SIZE);
-                val_slice.data = random_string(KEY_SIZE);
+                k = random_string(KEY_SIZE);
+                v = random_string(KEY_SIZE);
+
+                key_slice.data = &k[0];
+                val_slice.data = &v[0];
+
+                std::cout << "[PUT] key: " << key_slice.data
+                          << " val: " << val_slice.data << " ";
 
                 if (kv.put(key_slice, val_slice)) {
-                    std::cout << "[PUT] key: " << key_slice.data
-                              << " val: " << val_slice.data << " succeeded\n";
+                    std::cout << "succeeded\n";
                     m[key_slice] = val_slice;
                 } else {
-                    std::cout << "[PUT] key: " << key_slice.data
-                              << " val: " << val_slice.data << " failed\n";
+                    std::cout << "failed\n";
                 }
+                break;
             case 2:
                 // test kvStore->del
+                std::cout << "[DEL] key: " << key_slice.data
+                          << " val: " << val_slice.data;
+
                 if (m.size()) {
-                    key_slice.data = m[m.begin() + rand() % m.size() - 1];
+                    auto it = getRandomIterator(m);
+                    key_slice.data = it->second.data;
+
                     if (kv.del(key_slice)) {
-                        std::cout << "[DEL] key: " << key_slice.data
-                                  << " val: " << val_slice.data
-                                  << " succeeded\n";
+                        std::cout << "succeeded\n";
                         m.erase(key_slice);
                     } else {
-                        std::cout << "[DEL] key: " << key_slice.data
-                                  << " val: " << val_slice.data << " failed\n";
+                        std::cout << "failed\n";
                     }
                 } else {
                     std::cout << "[DEL] store empty\n";
                 }
+                break;
             case 3:
                 // test kvStore->get at index
+                int get_index = getRandomIndex(m);
+                std::cout << "[GET] "
+                          << "index: " << get_index
+                          << " key: " << key_slice.data
+                          << " val: " << val_slice.data << " ";
+
                 if (m.size()) {
-                    int get_index = rand() % n.size();
                     if (kv.get(get_index, key_slice, val_slice)) {
-                        if (m[m.begin() + get_index - 1] == val_slice)
-                            std::cout << "[GET] "
-                                      << "index: " << get_index
-                                      << " key: " << key_slice.data
-                                      << " val: " << val_slice.data
-                                      << " succeeded\n";
+                        auto it = getNthIterator(m, get_index);
+
+                        if (it->second == val_slice)
+                            std::cout << "succeeded\n";
                         else
-                            std::cout << "[GET] "
-                                      << "index: " << get_index
-                                      << " key: " << key_slice.data
-                                      << " val: " << val_slice.data
-                                      << " mismatch\n";
+                            std::cout << "mismatch\n";
                     } else {
-                        std::cout << "[GET] "
-                                  << "index: " << get_index
-                                  << " key: " << key_slice.data
-                                  << " val: " << val_slice.data << " failed\n";
+                        std::cout << "failed\n";
                     }
                 } else {
-                    std::cout << "[DEL] store empty\n";
+                    std::cout << "[GET] store empty\n";
                 }
+                break;
             case 4:
                 // test kvStore->del at index
+                int del_index = getRandomIndex(m);
+                std::cout << "[DEL] "
+                          << "index: " << del_index
+                          << " key: " << key_slice.data
+                          << " val: " << val_slice.data << " ";
+
                 if (m.size()) {
-                    int del_index = rand() % m.size();
                     // kv->get needed for debug logs
                     if (kv.get(del_index, key_slice, val_slice) &&
                         kv.del(del_index)) {
-                        std::cout << "[DEL] "
-                                  << "index: " << get_index
-                                  << " key: " << key_slice.data
-                                  << " val: " << val_slice.data
-                                  << " succeeded\n";
-                        m.erase(m.begin() + del_index - 1);
+                        std::cout << "succeeded\n";
+                        m.erase(getNthIterator(m, del_index));
                     }
                 } else {
-                    std::cout << "[DEL] "
-                              << "index: " << get_index
-                              << " key: " << key_slice.data
-                              << " val: " << val_slice.data << " failed\n";
+                    std::cout << "failed\n";
                 }
         }
     }
