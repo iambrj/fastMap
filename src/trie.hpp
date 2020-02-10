a2d9a93e4cfbe4607d4ac46dd3f4442a5a2b0f39
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-10 15:19:23 +0530

    more changes

--- src/trie.hpp
+++ src/trie.hpp
@@ -5,6 +5,12 @@ const int RANGE = 52;
 
 template <typename T>
 class TrieNode {
+    /*
+     * Each trie node attempts to store the values of the transition edges
+     * The transition "c" from a->b is represented by storing "c" on the node b,
+     * such that node b is at the index c[0]
+     */
+
     int idx;         // index of the first character of this string
     int len;         // length of the string data contained in this node
     char *value;     // value associated with the key ending at this trie node
@@ -18,6 +24,9 @@ class TrieNode {
         return c - 'a';
     }
 
+    void split() {
+    }
+
    public:
     TrieNode(char *str, int len) {
         idx = getIndex(*str);
@@ -26,37 +35,27 @@ class TrieNode {
         numofEnds = false;  // ?
     }
 
-    void insert(char *s, int len, char *value) {
-        int i = 0, idx = getIndex(*s);
+    // return whether value was overwritten or not
+    bool insert(char *s, int len, char *value) {
+        int idx = getIndex(*s);
 
-        // figure this out?!
         if (this->p[idx]) {
-            this->p[idx].insert(s + 1, len - 1);
+            // if the transition already exists
+            if (this->p[idx]->len == 1)
+                // if the transition stores only the first character
+                this->p[idx].insert(s + 1, len - 1);
+            else
+                ;
         } else {
+            // if the transition doesn't exist
             this->p = new TrieNode(s, len);
         }
-
-        currNode->numofEnds++;
     }
 
     /**
      * Precondition: s exists in Trie before erasure
      */
     void erase(char *s) {
-        TrieNode *currNode = this;
-        TrieNode *prevNode = this;
-
-        int i = 0, a;
-
-        while (i < sz(s)) {
-            a = getIndex(s[i]);
-            prevNode = currNode;
-            currNode = currNode->p[a];
-            i++;
-        }
-
-        currNode->numofEnds--;
-        if (currNode->numofEnds == 0)
-            prevNode->p[a] = NULL;
+        // need to rewrite
     }
 };
