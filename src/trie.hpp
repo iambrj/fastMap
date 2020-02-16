2072208499d227b742d5a9fa3b38399a912aaaea
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-16 12:02:45 +0530

    improved insert method

--- src/trie.hpp
+++ src/trie.hpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <assert.h>
+#include <stdlib.h>
 using namespace std;
 
 const int RANGE = 52;
@@ -11,7 +13,7 @@ class TrieNode {
      * such that node b is at the index c[0]
      */
 
-    int len;  // length of the string data contained in this node
+    int len;  // length of the string "data" contained in this node
     char *data;
     char *value;   // value associated with the key ending at this trie node
     TrieNode **p;  // array of TrieNode pointers
@@ -34,57 +36,74 @@ class TrieNode {
 
         int splittedOutLen = len - index;
 
-        TrieNode *splittedOut =
-            new TrieNode(s + index, splittedOutLen, this->value);
+        TrieNode *splittedOut = new TrieNode();
 
-        len -= splittedOutLen;
+        splittedOut->data = data + index;
+        splittedOut->len = splittedOutLen;
         // transfer value to the splitted out node
         splittedOut->value = value;
         value = nullptr;
 
+        // this node's remaining length
+        len -= splittedOutLen;
+
         //  pointer remapping
-        int newTransitionIndex = getIndex(s[index]);
+        int newTransitionIndex = getIndex(data[index]);
         TrieNode **previousTransitions = this->p;
         this->p = getNewTransitionsArray();
         this->p[newTransitionIndex] = splittedOut;
         splittedOut->p = previousTransitions;
     }
 
+    /* I DON'T THINK IT IS NECESSARY THOUGH */
+    // @description
+    // if this trie node has only one outward
+    // edge at the "index", then merge it back inside
+    void merge() {
+    }
+
    public:
-    TrieNode(char *str, int len, char *value) {
-        this->len = len;
+    TrieNode() {
+        data = nullptr;
+        value = nullptr;
+        len = 0;
+    }
+    TrieNode(char *str, int sLen, char *valueLen) {
+        this->len = sLen;
         p = getNewTransitionsArray();
         data = str;
-        this->value = value;
+        this->value = valueLen;
     }
 
     // return whether value was overwritten or not
-    bool insert(char *s, int len, char *value) {
-        int idx = getIndex(*s);
+    bool insert(char *s, int sLen, char *valueLen) {
+        int i = 0;
+        char *pointer = this->data;
 
-        if (this->p[idx]) {
-            int i = 0;
-            int len2 = this->p[idx]->len;
-            char *pointer = this->p[idx]->data;
-
-            while (i < len && i < len2 && *pointer == *s) {
-                s++;
-                i++;
-                pointer++;
-                len--;
-            }
-
-            if (i == len) {
-                // the existing transition completely encloses my transition
-                // so i need to split this node
-                this->p[idx]->split();
-            }
-
-            return this->p[idx]->insert(s, len, value);
-        } else {
-            // if the transition doesn't exist
-            this->p = new TrieNode(s, len, value);
+        while (i < sLen && i < len && *pointer == *s) {
+            s++;
+            i++;
+            pointer++;
+            sLen--;
         }
+
+        if (i == sLen) {
+            // both ending at same point, overwrite!
+            if (i == len) {
+                this->value = valueLen;
+                return true;
+            }
+
+            // The existing transition completely encloses my transition
+            // so i need to split this node
+            this->split(i);
+            int idx = getIndex(*s);
+            this->p[idx] = new TrieNode(s, sLen, valueLen);
+            return false;
+        }
+
+        int idx = getIndex(*s);
+        return this->p[idx]->insert(s, sLen, valueLen);
     }
 
     /**
