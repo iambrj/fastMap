4a986960662e526892506ec2473fd79e898283c1
GaurangTandon <gaurang.tandon@students.iiit.ac.in>
2020-02-17 10:57:20 +0530

    Add checker for input file, it compiles

--- src/fast_map.hpp
+++ src/fast_map.hpp
@@ -3,36 +3,39 @@
 
 struct Slice {
     uint8_t size;
-    char*   data;
+    char *data;
 };
 
 class kvStore {
-    private:
-        TrieNode root;
-    public:
-        kvStore(uint64_t max_entries);
-        //returns false if key didn’t exist
-        bool get(Slice &key, Slice &value) {
-            TrieNode *found = root.lookup(key.data, key.size, value.data);
-            if(!found)
-                return false;
-            value.size = strlen(value.data);
-            return true;
-        }
-        //returns true if value overwritten
-        bool put(Slice &key, Slice &value) {
-            return root.insert(key.data, value.size, value.data);
-        }
-        bool del(Slice &key) {
-            Slice value;
-            TrieNode *found = root.lookup(key.data, key.size, value.data);
-            if(!found)
-                return false;
-            root.erase(key.data);
-            return true;
-        }
-        //returns Nth key-value pair
-        bool get(int N, Slice &key, Slice &value);
-        //delete Nth key-value pair
-        bool del(int N);
+   private:
+    TrieNode root;
+
+   public:
+    kvStore(uint64_t max_entries) {
+        root = TrieNode();
+    }
+    // returns false if key didn’t exist
+    bool get(Slice &key, Slice &value) {
+        TrieNode *found = root.lookup(key.data, key.size, value.data);
+        if (!found)
+            return false;
+        value.size = strlen(value.data);
+        return true;
+    }
+    // returns true if value overwritten
+    bool put(Slice &key, Slice &value) {
+        return root.insert(key.data, value.size, value.data);
+    }
+    bool del(Slice &key) {
+        Slice value;
+        TrieNode *found = root.lookup(key.data, key.size, value.data);
+        if (!found)
+            return false;
+        root.erase(key.data);
+        return true;
+    }
+    // returns Nth key-value pair
+    bool get(int N, Slice &key, Slice &value);
+    // delete Nth key-value pair
+    bool del(int N);
 };
--- src/trie.cpp
+++ src/trie.cpp
@@ -1,10 +1,16 @@
+#include <fstream>
+#include <map>
+
 #include <string>
 #include <string.h>
 #include <vector>
-#include "trie.hpp"
+#include "fast_map.hpp"
 using namespace std;
+#define LOOKUP_OP 0
+#define INSERT_OP 1
+#define ERASE_OP 2
 
-int main() {
+void naiveCheck() {
     TrieNode *root = new TrieNode();
     vector<char *> strs = {"abcd", "efgh", "abdef"};
     vector<char *> values = {"vals", "tree", "abcd"};
@@ -33,4 +39,117 @@ int main() {
             printf("%s\n", value);
         }
     }
+}
+
+char *getCharPointer(string &s) {
+    char *valueChar = (char *)malloc(s.size());
+    for (int i = 0; i < s.size(); i++) {
+        *(valueChar + i) = s[i];
+    }
+
+    return valueChar;
+}
+
+void setStringIntoSlice(string &s, Slice &slc) {
+    slc.data = getCharPointer(s);
+    slc.size = s.size();
+}
+
+#define fail                                     \
+    {                                            \
+        printf("Mismatch at operation %d: ", i); \
+        return;                                  \
+    }
+void fileCheck() {
+    map<string, string> naive;
+    const char *FILE_PATH = "genInp.txt";
+
+    ifstream file(FILE_PATH);
+
+    int opCount;
+    file >> opCount;
+
+    kvStore fastMap((uint64_t)opCount);
+
+    for (int i = 1; i <= opCount; i++) {
+        int op;
+        file >> op;
+        string key;
+        file >> key;
+        string actual, value;
+        int found, wasFound, actuallyFound, isOverwrite;
+        Slice x, y, z;
+
+        switch (op) {
+            case LOOKUP_OP:
+                actual = naive[key];
+                actuallyFound = actual.size() > 0;
+
+                setStringIntoSlice(key, x);
+
+                wasFound = fastMap.get(x, y);
+
+                if (wasFound != actuallyFound) {
+                    fail;
+                }
+
+                if (wasFound) {
+                    if (!strcmp(actual.c_str(), y.data)) {
+                        fail;
+                    }
+                }
+
+                break;
+            case INSERT_OP:
+                file >> value;
+
+                isOverwrite = (naive[key].size() > 0);
+                naive[key] = value;
+
+                setStringIntoSlice(key, x);
+                setStringIntoSlice(value, y);
+
+                found = fastMap.get(x, y);
+
+                if (found != isOverwrite)
+                    fail;
+
+                fastMap.put(x, y);
+
+                found = fastMap.get(x, z);
+
+                if (!found) {
+                    fail;
+                }
+
+                if (!strcmp(value.c_str(), z.data)) {
+                    fail;
+                }
+
+                break;
+            case ERASE_OP:
+                naive.erase(naive.find(key));
+
+                setStringIntoSlice(key, x);
+
+                // check if erased correctly from fastmap
+                found = fastMap.del(x);
+
+                if (!found)
+                    fail;
+
+                found = fastMap.get(x, y);
+
+                if (found)
+                    fail;
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+int main() {
 }
