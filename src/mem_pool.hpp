d8613507101f35da0d54399cc9ee2840e6833864
iambrj <joshibharathiramana@gmail.com>
2020-02-28 16:26:06 +0530

    Implement basic memory pool

---
+++ src/mem_pool.hpp
@@ -0,0 +1,37 @@
+#ifndef _MEM_POOL_
+#define _MEM_POOL_
+
+#include <cstdlib>
+class mem_pool {
+    typedef unsigned long long ull;
+    private:
+        char *head, *cur, *end;
+        ull unit_size;
+    public:
+        mem_pool(ull MAX_BYTES, ull unit_size) {
+            head = (char *) malloc(MAX_BYTES);
+            cur = head;
+            end = head + MAX_BYTES;
+            this->unit_size = unit_size;
+        }
+        void *get_units(ull count) {
+            if (cur + count * this->unit_size >= end)
+                return NULL;
+            
+            void *units = cur;
+            cur += count * this->unit_size;
+            return units;
+        }
+        bool free_units(ull count) {
+            if (cur - count * this->unit_size < head)
+                return false;
+
+            cur -= count * this->unit_size;
+            return true;
+        }
+        void free_pool() {
+            return free(head);
+        }
+};
+
+#endif
